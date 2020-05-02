7aa3de52bdd415e21fed065907e815f98320f223
Yuri Abibok <yuriabibok@gmail.com>
2020-05-02 14:42:22 +0300

    Add more unit tests

---
+++ Yotec.Api.Tests/Services/ArticleHttpClientTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using Yotec.Api.Models;
+using Yotec.Api.Services;
+
+namespace Yotec.Api.Tests.Services
+{
+    [TestFixture]
+    public class ArticleHttpClientTests
+    {
+        [Test]
+        public async Task IsAvailableAsync_Available_ReturnTrue()
+        {
+            var httpClient = MockHttpClient(HttpStatusCode.OK, "");
+            var mapperMock = new Mock<IMapper>();
+            var articleClient = new ArticleHttpClient(httpClient, mapperMock.Object);
+
+            var result = await articleClient.IsAvailableAsync();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task IsAvailableAsync_NotAvailable_ReturnFalse()
+        {
+            var httpClient = MockHttpClient(HttpStatusCode.ServiceUnavailable, "");
+            var mapperMock = new Mock<IMapper>();
+            var articleClient = new ArticleHttpClient(httpClient, mapperMock.Object);
+
+            var result = await articleClient.IsAvailableAsync();
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task GetItemsAsync_Valid_ReturnResponse()
+        {
+            var content =
+                "{\"results\":[{\"title\":\"Some title\",\"updated_date\":\"2020-05-02T06:40:13-04:00\",\"short_url\":\"https://nyti.ms/dfrty\"}]}";
+            var expectedResult = new List<ArticleView>
+            {
+                new ArticleView
+                {
+                    Heading = "Some title",
+                    Updated = DateTime.Parse("2020-05-02T06:40:13-04:00"),
+                    Link = "https://nyti.ms/dfrty"
+                }
+            };
+            var httpClient = MockHttpClient(HttpStatusCode.OK, content);
+            var mapperMock = new Mock<IMapper>();
+            mapperMock
+                .Setup(x => x.Map<IEnumerable<ArticleView>>(It.IsAny<object>()))
+                .Returns(expectedResult);
+            var articleClient = new ArticleHttpClient(httpClient, mapperMock.Object);
+
+            var result = await articleClient.GetItemsAsync("section");
+
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [Test]
+        public void GetItemsAsync_ServerIsNotAvailable_ThrowException()
+        {
+            var httpClient = MockHttpClient(HttpStatusCode.ServiceUnavailable, "", "Something went wrong");
+            var mapperMock = new Mock<IMapper>();
+            var articleClient = new ArticleHttpClient(httpClient, mapperMock.Object);
+
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await articleClient.GetItemsAsync("section");
+            }, "Error was occured during connection to server. Code: 503. Reason: Something went wrong");
+        }
+
+        private HttpClient MockHttpClient(HttpStatusCode code, string content, string reason = null)
+        {
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = code,
+                    Content = new StringContent(content),
+                    ReasonPhrase = reason
+                })
+                .Verifiable();
+
+            return new HttpClient(handlerMock.Object);
+        }
+    }
+}
---
+++ Yotec.Api.Tests/Services/ArticleServiceTests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Yotec.Api.Helpers;
+using Yotec.Api.Models;
+using Yotec.Api.Services;
+
+namespace Yotec.Api.Tests.Services
+{
+    [TestFixture]
+    public class ArticleServiceTests
+    {
+        private readonly IEnumerable<ArticleView> articles = new List<ArticleView>
+        {
+            new ArticleView
+            {
+                Heading = "Title1",
+                Updated = DateTime.Parse("2020-05-01T06:40:13-04:00"),
+                Link = "https://nyti.ms/title1"
+            },
+            new ArticleView
+            {
+                Heading = "Title2",
+                Updated = DateTime.Parse("2020-05-02T06:40:13-04:00"),
+                Link = "https://nyti.ms/title2"
+            },
+            new ArticleView
+            {
+                Heading = "Title3",
+                Updated = DateTime.Parse("2020-05-03T06:40:13-04:00"),
+                Link = "https://nyti.ms/title3"
+            },
+            new ArticleView
+            {
+                Heading = "Title4",
+                Updated = DateTime.Parse("2020-05-01T06:40:13-04:00"),
+                Link = "https://nyti.ms/title4"
+            },
+            new ArticleView
+            {
+                Heading = "Title5",
+                Updated = DateTime.Parse("2020-05-01T06:40:13-04:00"),
+                Link = "https://nyti.ms/title5"
+            }
+        };
+
+        [Test]
+        public async Task CheckAvailabilityAsync_ServerAvailable_ReturnSuccessString()
+        {
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.IsAvailableAsync()).ReturnsAsync(true);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var result = await articleService.CheckAvailabilityAsync();
+
+            Assert.AreEqual("Service is available.", result);
+        }
+
+        [Test]
+        public async Task CheckAvailabilityAsync_ServerNotAvailable_ReturnErrorString()
+        {
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.IsAvailableAsync()).ReturnsAsync(false);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var result = await articleService.CheckAvailabilityAsync();
+
+            Assert.AreEqual("Service is not available. Please contact to administrator.", result);
+        }
+
+        [Test]
+        public void GetSectionItemsAsync_SectionArgNull_ThrowException()
+        {
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await articleService.GetSectionItemsAsync(null);
+            });
+        }
+
+        [Test]
+        public async Task GetSectionItemsAsync_Valid_ResurnResponse()
+        {
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.GetItemsAsync("section")).ReturnsAsync(articles);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var response = await articleService.GetSectionItemsAsync("section");
+
+            Assert.AreEqual(articles, response);
+        }
+
+        [Test]
+        public async Task GetFirstSectionItemAsync_Valid_ResurnResponse()
+        {
+            var expectedResult = new ArticleView
+            {
+                Heading = "Title1",
+                Updated = DateTime.Parse("2020-05-01T06:40:13-04:00"),
+                Link = "https://nyti.ms/title1"
+            };
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.GetItemsAsync("section")).ReturnsAsync(articles);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var response = await articleService.GetFirstSectionItemAsync("section");
+
+            Assert.AreEqual(SerializeHelper.Serialize(expectedResult), SerializeHelper.Serialize(response));
+        }
+
+        [Test]
+        public async Task GetSectionItemsByDateAsync_Valid_ResurnResponse()
+        {
+            var expectedResult = new List<ArticleView>
+            {
+                new ArticleView
+                {
+                    Heading = "Title2",
+                    Updated = DateTime.Parse("2020-05-02T06:40:13-04:00"),
+                    Link = "https://nyti.ms/title2"
+                }
+            };
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.GetItemsAsync("section")).ReturnsAsync(articles);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var response = await articleService.GetSectionItemsByDateAsync("section", "2020-05-02");
+
+            Assert.AreEqual(SerializeHelper.Serialize(expectedResult), SerializeHelper.Serialize(response));
+        }
+
+        [Test]
+        public async Task GetSectionItemByShortUrlAsync_Valid_ResurnResponse()
+        {
+            var expectedResult = new ArticleView
+            {
+                Heading = "Title3",
+                Updated = DateTime.Parse("2020-05-03T06:40:13-04:00"),
+                Link = "https://nyti.ms/title3"
+            };
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.GetItemsAsync("section")).ReturnsAsync(articles);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var response = await articleService.GetSectionItemByShortUrlAsync("title3");
+
+            Assert.AreEqual(SerializeHelper.Serialize(expectedResult), SerializeHelper.Serialize(response));
+        }
+
+        [Test]
+        public async Task GetSectionItemsGroupedByDateAsync_Valid_ResurnResponse()
+        {
+            var expectedResult = new List<ArticleGroupByDateView>
+            {
+                new ArticleGroupByDateView
+                {
+                    Date = "2020-05-01",
+                    Total = 3
+                },
+                new ArticleGroupByDateView
+                {
+                    Date = "2020-05-02",
+                    Total = 1
+                },
+                new ArticleGroupByDateView
+                {
+                    Date = "2020-05-03",
+                    Total = 1
+                },
+            };
+            var articleHttpClientMock = new Mock<IArticleHttpClient>();
+            articleHttpClientMock.Setup(x => x.GetItemsAsync("section")).ReturnsAsync(articles);
+            var articleService = new ArticleService(articleHttpClientMock.Object);
+
+            var response = await articleService.GetSectionItemsGroupedByDateAsync("section");
+
+            Assert.AreEqual(SerializeHelper.Serialize(expectedResult), SerializeHelper.Serialize(response));
+        }
+    }
+}
